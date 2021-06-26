2adca7e11fb05464e3546f5aed27590b63aa6552
pkn0814 <nayapark0814@gmail.com>
2021-06-27 00:28:12 +0900

    7주차과제

--- fifthproject/settings.py
+++ fifthproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -32,13 +33,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blogApp',
+    'photo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'photo', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- fifthproject/urls.py
+++ fifthproject/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import blogApp.views
 import accounts.views
+import photo.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',blogApp.views.home, name='home'),
     path('blog/<int:blog_id>', blogApp.views.detail, name='detail'),
     path('accounts/', include('accounts.urls')),
-]
+    path('photo/', photo.views.photo, name='photo'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
